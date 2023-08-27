487723a1d92a650b59fbc4352b82b373f781cbcc
LAMAS Fernando <fersum414@gmail.com>
2023-08-27 16:32:51 -0300

    correcion form

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -5,7 +5,7 @@ $form.addEventListener('submit', handleSubmit)
 
 async function handleSubmit(event) {
   event.preventDefault()
-  const form = new FormData(this)
+const form = new FormData(this)
 const response = await fetch(this.action, {
 method: this.method,
 body: form,
