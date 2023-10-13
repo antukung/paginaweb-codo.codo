7a593d2d5b623aba5c983a286efa119a342929e6
Antu <122485403+antukung@users.noreply.github.com>
2023-10-13 20:06:46 -0300

    Fer elimina archivos

--- javascript/app.js
+++ javascript/app.js
@@ -2,89 +2,89 @@ const databaselibrosJSON= [
   {  
     "_id": 1,
     "nombre":"Harry Poter y La Piedra Filosofal",
-    "imagen":"/img/libro1.png"
+    "imagen":"../img/Libro1.png"
   },
   { 
     "_id": 2,
     "nombre":"Harry Poter y La Camara Secreta",
-    "imagen":"/img/libro2.png"
+    "imagen":"../img/Libro2.png"
   },
   { 
     "_id": 3,
     "nombre":"Harry Poter y El Prisionero de Azkaban",
-    "imagen":"/img/libro3.png"
+    "imagen":"../img/Libro3.png"
   }
   ,
   { 
     "_id": 4,
     "nombre":"Harry Poter y El Caliz del Fuego",
-    "imagen":"/img/libro4.png"
+    "imagen":"../img/Libro4.png"
   }
   ,
   { 
     "_id": 5,
     "nombre":"Harry Poter y La Orden del Fenix",
-    "imagen":"/img/libro5.png"
+    "imagen":"../img/Libro5.png"
   }
   ,
   { 
     "_id": 6,
     "nombre":"Harry Poter y El Principe Mestizo",
-    "imagen":"/img/libro6.png"
+    "imagen":"../img/Libro6.png"
   }
   ,
   { 
     "_id": 7,
     "nombre":"Harry Poter y Las Reliquias de la Muerte ",
-    "imagen":"/img/libro7.png"
+    "imagen":"../img/Libro7.png"
   }
   ,
   { 
     "_id": 8,
     "nombre":"Animales Fantasticos y Donde Encontrarlos",
-    "imagen":"/img/libro8.png"
+    "imagen":"../img/Libro8.png"
   }
   ,
   { 
     "_id": 9,
     "nombre":"James Poter y La Encrucijada de Los Mayores",
-    "imagen":"/img/libro9.png"
+    "imagen":"../img/Libro9.png"
   }
   ,
   { 
     "_id": 10,
     "nombre":"James Poter y La Maldicion del Guardian",
-    "imagen":"/img/libro10.png"
+    "imagen":"../img/Libro10.png"
   }
   ,
   { 
     "_id": 11,
     "nombre":"James Poter y La Bobeda de los destinos",
-    "imagen":"/img/libro11.png"
+    "imagen":"../img/Libro11.png"
   }
   ,
   { 
     "_id": 12,
     "nombre":"James Poter y la Red Morigan",
-    "imagen":"/img/libro12.png"
+    "imagen":"../img/Libro12.png"
   }
   ,
   { 
     "_id": 13,
     "nombre":"James Poter y El Hilo Carmesi",
-    "imagen":"/img/libro13.png"
+    "imagen":"../img/Libro13.png"
   }
   ,
   { 
     "_id": 14,
     "nombre":"La Primera Navidad de Harry y Otros Cuentos",
-    "imagen":"/img/libro14.png"
+    "imagen":"../img/Libro14.png"
   }
   ,
   { 
     "_id": 15,
     "nombre":"La Chica del Embarcadero",
-    "imagen":"/img/libro15.png"
+    "imagen":"../img/Libro15.png"
   }
 
 ]
--- javascript/constructorlibros.js
+++ javascript/constructorlibros.js
@@ -1,25 +1,23 @@
-const divLibros = document.getElementById("libros-argentina");
-const inputNombre = document.getElementById("input-nombre");
+const divLibros = document.querySelector(".libros-argentina");
 
-divLibros.innerHTML=""
 
-for (const libro of databaselibrosJSON) {
 
-        const div = document.createElement("div");
-        div.classList.add("libro");
-      
-        const imagen = document.createElement("img");
-        imagen.src = libro.imagen;
-      
-        const titulo = document.createElement("p");
-        titulo.textContent = libro.nombre;
-      
-        // Agrega la imagen y el título al div.
-        libro.appendChild(imagen);
-        libro.appendChild(titulo);
-      
-        // Agrega el div al contenedor.
-        div.appendChild(div);
-    
-        
+for (var i = 0; i < databaselibrosJSON.length; i++) {
+  const div = document.createElement("div");
+  div.classList.add("libro");
+  
+  
+  const nombre = document.createElement("p");
+  nombre.textContent = databaselibrosJSON[i].nombre;
+  nombre.classList.add("nombreDelLibro");
+  
+  
+  const imagen = document.createElement("img");
+  imagen.src = databaselibrosJSON[i].imagen;
+  imagen.classList.add("imagenDelLibro");
+  
+  div.appendChild(nombre);
+  div.appendChild(imagen);
+  
+  divLibros.appendChild(div);
 }
