7e5aa55fd6e60c2d120d08cfb3ccfd9ef91cd117
Antu <122485403+antukung@users.noreply.github.com>
2023-10-17 01:10:39 -0300

    fer despues hablamos

--- javascript/app.js
+++ javascript/app.js
@@ -101,5 +101,83 @@ const databaselibrosJSON= [
     "imagen":"../img/Libro15.png",
     "enlace":"https://mega.nz/file/Z8gwCAzZ#QGQWWyDlwIp3zsHo2CNH1NyCEaJ0Hl7BNuwYJQE0DWA"
   }
+  ,
+  { 
+    "_id": 16,
+    "nombre":"Tao",
+    "imagen":"../img/Libro16.png",
+    "enlace":"https://mega.nz/file/4kgWjSrQ#_To9vMti7DUKZkstySBYbcbeDNcBxtuGsZ_KqzsN7FM"
+  }
+  ,
+  { 
+    "_id": 17,
+    "nombre":"Yo Cuento de Hada Jara Sirkin",
+    "imagen":"../img/Libro17.png",
+    "enlace":"https://mega.nz/file/45hlhDpb#WEH3sMao_yp53fzSHMCHl_JkU_uSW7CTV3_AceJCYZ0"
+  }
+  ,
+  { 
+    "_id": 18,
+    "nombre":"Herejia Bermeja",
+    "imagen":"../img/Libro18.png",
+    "enlace":"https://mega.nz/file/lthECLIC#70_tdqUBVcVlrvlHyP1oe84HNiRlMbxjdz6ElaWHgbI"
+  }
+  ,
+  { 
+    "_id": 19,
+    "nombre":"Relectura (en viaje)",
+    "imagen":"../img/Libro19.png",
+    "enlace":"https://mega.nz/file/U0JzHBpD#Hzgx8ATpew4hnnE9k0zut-HBmEZN0enzjIEDJ9ho9XA"
+  }
+  ,
+  { 
+    "_id": 20,
+    "nombre":"El Nacimiento de laa Filosofia",
+    "imagen":"../img/Libro20.png",
+    "enlace":"https://mega.nz/file/d0plnYgZ#cSNJz8EfrciDGGJOs7Vedb6Wd2PK-CxIY1FmJxYlgiI"
+  }
+  ,
+  { 
+    "_id": 21,
+    "nombre":"Los Dia Del Venado",
+    "imagen":"../img/Libro21.png",
+    "enlace":"https://mega.nz/file/Y8I2nDJR#G7h2TbF5fAFeyGyAem3hLKxFZITourVZknoTXLaxiho"
+  }
+  ,
+  { 
+    "_id": 22,
+    "nombre":"Los dias De La Sombra",
+    "imagen":"../img/Libro26.png",
+    "enlace":"https://mega.nz/file/B0wFzAIS#6BIfbSBT2s37wiIkc2iLq4m3IZANbKOI7-CXfq72I0Q"
+  }
+  ,
+  { 
+    "_id": 23,
+    "nombre":"El Coraje de La Verdad Foucault",
+    "imagen":"../img/Libro22.png",
+    "enlace":"https://mega.nz/file/ktgyTAJD#wkYwvFK6qGrGeAupTyr1QWQMfe37eBSnDX659amqcag"
+  }
+  ,
+  { 
+    "_id": 24,
+    "nombre":"El Orden del Discurso Foucault",
+    "imagen":"../img/Libro23.png",
+    "enlace":"https://mega.nz/file/xwQkyTqI#FT9ZQW6WvSZvLxaanUWesEPu_v4Zx5Vtt9oM9Ap4bKc"
+  }
+  ,
+  { 
+    "_id": 25,
+    "nombre":"Enfermedad Mental y Personalidad Foucault",
+    "imagen":"../img/Libro24.png",
+    "enlace":"https://mega.nz/file/s9ZSyTxK#oTs4mgjD7Q00gZgs9w6VOdEqvZ7czutNnIrfKAubpjo"
+  }
+  ,
+  { 
+    "_id": 26,
+    "nombre":"Relato de Una Soñadora",
+    "imagen":"../img/Libro25.png",
+    "enlace":"https://mega.nz/file/clZXEbgS#vE2hWJ2TYvYBxPQxOfwO6VcC_XmNLpI_wj6ShpPSUmA"
+  }
+  
 
 ]
