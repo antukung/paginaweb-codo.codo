3c8c8e381c2eb65e87545c1b89a41b910aee9755
Antu <122485403+antukung@users.noreply.github.com>
2023-10-16 22:28:51 -0300

    le falta vaste y no se como centrar los libros
    chicos no se como centrar los libros si lo descubre nos salvaria la vida estamos muy complicados

--- javascript/app.js
+++ javascript/app.js
@@ -2,89 +2,104 @@ const databaselibrosJSON= [
   {  
     "_id": 1,
     "nombre":"Harry Poter y La Piedra Filosofal",
-    "imagen":"../img/Libro1.png"
+    "imagen":"../img/Libro1.png",
+    "enlace":"https://mega.nz/file/d0hFDDwD#rcw8tcK1U0gTyqgqt0-JLIqeTeAv4r-vQu-_044_9sc"
   },
   { 
     "_id": 2,
     "nombre":"Harry Poter y La Camara Secreta",
-    "imagen":"../img/Libro2.png"
+    "imagen":"../img/Libro2.png",
+    "enlace":"https://mega.nz/file/RhRDFaTD#SIjrObGh9IhugeAAjPnHFRsIcfaFGLfmHmxrpiei0mU"
   },
   { 
     "_id": 3,
     "nombre":"Harry Poter y El Prisionero de Azkaban",
-    "imagen":"../img/Libro3.png"
+    "imagen":"../img/Libro3.png",
+    "enlace":"https://mega.nz/file/p0BhhBwR#znYOs5peuCcMeOUNd55OqkJ05mmjWJes6aVgICk16NU"
   }
   ,
   { 
     "_id": 4,
     "nombre":"Harry Poter y El Caliz del Fuego",
-    "imagen":"../img/Libro4.png"
+    "imagen":"../img/Libro4.png",
+    "enlace":"https://mega.nz/file/xhJxibgL#_5NWIlWEHvYhu3FpqYXzGWe_5pUJ_-yCAJBxOWS_ngc"
   }
   ,
   { 
     "_id": 5,
     "nombre":"Harry Poter y La Orden del Fenix",
-    "imagen":"../img/Libro5.png"
+    "imagen":"../img/Libro5.png",
+    "enlace":"https://mega.nz/file/x8ggASRJ#4b9WE1OmfPFx23EDo6qTkU50wPBL11zaUDWCU9OzGg4"
   }
   ,
   { 
     "_id": 6,
     "nombre":"Harry Poter y El Principe Mestizo",
-    "imagen":"../img/Libro6.png"
+    "imagen":"../img/Libro6.png",
+    "enlace":"https://mega.nz/file/EwYkWICb#FrTShfDrkA17RxV0xmdQ_t3RFHbc3uv90fw24KPT16E"
   }
   ,
   { 
     "_id": 7,
     "nombre":"Harry Poter y Las Reliquias de la Muerte ",
-    "imagen":"../img/Libro7.png"
+    "imagen":"../img/Libro7.png",
+    "enlace":"https://mega.nz/file/tk4hBJ6Z#ntRJKC5VmPlw_i17KxobdIf7jjY6T1Y9G_Zo20seDH8"
   }
   ,
   { 
     "_id": 8,
     "nombre":"Animales Fantasticos y Donde Encontrarlos",
-    "imagen":"../img/Libro8.png"
+    "imagen":"../img/Libro8.png",
+    "enlace":"https://mega.nz/file/x5wihQhA#CbaMfr8ij5Sf0-7Q4BrnDLeRZ3p-7yoPw-S1Mu9dg34"
   }
   ,
   { 
     "_id": 9,
     "nombre":"James Poter y La Encrucijada de Los Mayores",
-    "imagen":"../img/Libro9.png"
+    "imagen":"../img/Libro9.png",
+    "enlace":"https://mega.nz/file/hhgm2A7B#0FYe-iGwroL_wO9-4bTPb5cuoqRX4K0c9xJu7336m14"
   }
   ,
   { 
     "_id": 10,
     "nombre":"James Poter y La Maldicion del Guardian",
-    "imagen":"../img/Libro10.png"
+    "imagen":"../img/Libro10.png",
+    "enlace":"https://mega.nz/file/NxoH1DxL#9OLmR-_VxkhJH_8GW5x0RqINDtGrUSX5CS0XkgibPoM"
   }
   ,
   { 
     "_id": 11,
     "nombre":"James Poter y La Bobeda de los destinos",
-    "imagen":"../img/Libro11.png"
+    "imagen":"../img/Libro11.png",
+    "enlace":"https://mega.nz/file/Bx5y2YLT#2dts9I4Pe9NthKdlJ5RGKQlrxJHgyio4C_x9tAoxv3Y"
   }
   ,
   { 
     "_id": 12,
     "nombre":"James Poter y la Red Morigan",
-    "imagen":"../img/Libro12.png"
+    "imagen":"../img/Libro12.png",
+    "enlace":"https://mega.nz/file/4t5QxAxS#oz-SL0v1wSff7ecrZ6QLQFxQxGD7rBnJrutvUoc6aKY"
   }
   ,
   { 
     "_id": 13,
     "nombre":"James Poter y El Hilo Carmesi",
-    "imagen":"../img/Libro13.png"
+    "imagen":"../img/Libro13.png",
+    "enlace":"https://mega.nz/file/M1oUiYgY#ZGLMrgZtDwETsXUDwgS8TWYBXJu5pb33BdflAPWkFT8"
   }
   ,
   { 
     "_id": 14,
     "nombre":"La Primera Navidad de Harry y Otros Cuentos",
-    "imagen":"../img/Libro14.png"
+    "imagen":"../img/Libro14.png",
+    "enlace":"https://mega.nz/file/BoAAFZDK#Z3ATAfISsJrva6OGzNbYj0fe9kGVvpRVSmpKh-_u2Ps"
   }
   ,
   { 
     "_id": 15,
     "nombre":"La Chica del Embarcadero",
-    "imagen":"../img/Libro15.png"
+    "imagen":"../img/Libro15.png",
+    "enlace":"https://mega.nz/file/Z8gwCAzZ#QGQWWyDlwIp3zsHo2CNH1NyCEaJ0Hl7BNuwYJQE0DWA"
   }
 
 ]
--- javascript/constructorlibros.js
+++ javascript/constructorlibros.js
@@ -1,23 +1,27 @@
 const divLibros = document.querySelector(".libros-argentina");
 
 
-
+// Supongamos que tienes un objeto JSON llamado databaselibrosJSON
 for (var i = 0; i < databaselibrosJSON.length; i++) {
   const div = document.createElement("div");
   div.classList.add("libro");
-  
   
   const nombre = document.createElement("p");
   nombre.textContent = databaselibrosJSON[i].nombre;
   nombre.classList.add("nombreDelLibro");
   
-  
+  const enlaceImagen = document.createElement("a"); // Crear un enlace
+  enlaceImagen.href = databaselibrosJSON[i].enlace; // Establecer la URL de destino
+  enlaceImagen.target = "_blank"; // Abre el enlace en una nueva pestaña (opcional)
+
   const imagen = document.createElement("img");
   imagen.src = databaselibrosJSON[i].imagen;
   imagen.classList.add("imagenDelLibro");
-  
+
+  enlaceImagen.appendChild(imagen); // Agregar la imagen al enlace
+
+  div.appendChild(enlaceImagen); // Agregar el enlace que contiene la imagen
   div.appendChild(nombre);
-  div.appendChild(imagen);
   
   divLibros.appendChild(div);
 }
